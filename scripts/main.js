f831bd796dfbac235e62778f42cc8698b3dc06bc
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-10-03 18:56:28 +0800

    fn() is workiing and the notes app projet is done

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ export default function main(root) {
   const ui = UI(root, methodHandler());
 
   const notes = api.getNotes();
-  let activeNoteItem = notes[0];
+  let activeNoteItem = notes[notes.length - 1];
 
   init();
 
@@ -30,7 +30,13 @@ export default function main(root) {
       ui.updatePreviewVisibility(true);
     };
 
-    const onEdit = (title, description) => {};
+    const onEdit = (title, description) => {
+      activeNoteItem.title = title;
+      activeNoteItem.description = description;
+      api.addNote(activeNoteItem);
+      ui.updateNotesList(notes);
+      ui.updateSelectedNote(activeNoteItem);
+    };
 
     const onAdd = () => {
       const newNote = {
